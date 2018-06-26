aeb70b760b54da495e259c24ca42563cbde5a4eb
abbylee1 <ablee@micds.org>
2018-06-26 15:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function (){
 function searchRepositories(#searchTerms) {
   $("#searchTerms").val();
   $.get(`https://api.github.com/search/repositories?q=`
-    $(#searchTerms});
+    $(#searchTerms}); 
   
 }
   
